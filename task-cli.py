d0e6f5c14aec98a2eeef424e1b2cdd4ce41dff5c
Santhosh Datturi <dsanthosh.clg@gmail.com>
2024-12-31 14:33:28 +0530

    add add task feature

---
+++ task-cli.py
@@ -0,0 +1,60 @@
+import json
+import argparse
+import sys
+from enum import Enum
+from datetime import datetime
+from typing import List, Dict, Any
+
+TASK_FILE = 'tasks.json'
+
+class Status(Enum):
+    TODO = "todo"
+    IN_PROGRESS = "in-progress"
+    DONE = "done"
+
+class Command(Enum):
+    ADD = "add"
+    LIST = "list"
+    MARK = "mark"
+
+class Task(Enum):
+    ID = "id"
+    DESCRIPTION = "description"
+    STATUS = "status"
+    CREATED_AT = "createdAt"
+    UPDATED_AT = "updatedAt"
+
+
+def load_tasks() -> List[Dict[str, Any]]:
+    try:
+        with open(TASK_FILE, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+    
+def save_tasks(tasks: List[Dict[str, Any]]) -> None:
+    with open(TASK_FILE, 'w') as file:
+        json.dump(tasks, file, indent=4)
+
+def generate_task_id(tasks: List[Dict[str, Any]]) -> int:
+    return max((task['id'] for task in tasks), default=1) + 1
+
+def add_task(description: str) -> None:
+    tasks: List[Dict[str, Any]] = load_tasks()
+
+    task_id: int = generate_task_id(tasks)
+    time_now: str = datetime.now().isoformat()
+
+    new_task: dict = {
+        Task.ID.value: task_id,
+        Task.DESCRIPTION.value: description,
+        Task.STATUS.value: Status.TODO.value,
+        Task.CREATED_AT.value: time_now,
+        Task.UPDATED_AT.value: None
+    }
+
+    tasks.append(new_task)
+
+    save_tasks(tasks)
+
+
